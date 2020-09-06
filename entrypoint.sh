7041cc153013ea783bb97141ed677093d286d061
Jason Kulatunga <jason@thesparktree.com>
2020-09-06 13:03:56 -0700

    update entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
-#!/bin/bash -l
+#!/usr/bin/env bash
 
 PATH=/srv/packagr:/go/bin:/usr/local/go/bin:$PATH
-env
+
+# retrieve the latest bumpr binary
+asset_url=$(curl -s https://api.github.com/repos/PackagrIO/bumpr/releases/latest \
+	| grep browser_download_url | grep 'packagr-bumpr-linux-amd64' | cut -d '"' -f 4)
+
+# download the bumpr asset here.
+curl -L -o /srv/packagr/packagr-bumpr $asset_url
+
+# make bumpr executable
+chmod +x /srv/packagr/packagr-bumpr
+
+
 echo "Starting Bumpr $1"
-version=$(/srv/packagr/packagr-bumpr start --scm github --package_type golang)
-echo "::set-output name=version::$version"
+packagr-bumpr start --scm github --package_type golang
