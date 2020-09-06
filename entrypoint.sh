2b938cd97077b0af3fac9075c30cc224c046ba16
Jason Kulatunga <jason@thesparktree.com>
2020-09-06 10:27:12 -0700

    update path.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -l
 
-echo "::add-path::/srv/packagr"
-echo "::add-path::/go/bin"
-echo "::add-path::/usr/local/go/bin"
-
+PATH=/srv/packagr:/go/bin:/usr/local/go/bin:$PATH
 env
-
 echo "Starting Bumpr $1"
 version=$(/srv/packagr/packagr-bumpr start --scm github --package_type golang)
 echo "::set-output name=version::$version"
