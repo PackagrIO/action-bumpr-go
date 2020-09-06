045edfa5c85618713167d9cd1b2dc37dd6a4058d
Jason Kulatunga <jason@thesparktree.com>
2020-09-05 19:19:35 -0700

    fix bumpr command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
 echo "Starting Bumpr $1"
-version=$(/srv/packagr/packagr-bumpr --scm github --package_type golang)
+version=$(/srv/packagr/packagr-bumpr start --scm github --package_type golang)
 echo "::set-output name=version::$version"
