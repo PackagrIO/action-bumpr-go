96b6c61ce49f272c411812be2c948d26b28769b2
Jason Kulatunga <jason@thesparktree.com>
2021-06-18 14:17:32 -0700

    adding ability to change working directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,9 @@ curl -L -o /srv/packagr/packagr-bumpr $asset_url
 # make bumpr executable
 chmod +x /srv/packagr/packagr-bumpr
 
+if [[ ! -z "${CUSTOM_WORKING_DIRECTORY}" ]]; then
+  cd "${CUSTOM_WORKING_DIRECTORY}"
+fi
 
 echo "Starting Bumpr $1"
 packagr-bumpr start --scm github --package_type golang
