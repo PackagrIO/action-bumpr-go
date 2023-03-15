3af48198b3535051d262aa04153a2b0bc6f5883d
Jason Kulatunga <jason@thesparktree.com>
2023-03-14 19:04:24 -0700

    using bumpr repo docker image.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,6 @@
 
 PATH=/srv/packagr:/go/bin:/usr/local/go/bin:$PATH
 
-# retrieve the latest bumpr binary
-asset_url=$(curl -s https://api.github.com/repos/PackagrIO/bumpr/releases/latest \
-	| grep browser_download_url | grep 'packagr-bumpr-linux-amd64' | cut -d '"' -f 4)
-
-# download the bumpr asset here.
-curl -L -o /srv/packagr/packagr-bumpr $asset_url
-
-# make bumpr executable
-chmod +x /srv/packagr/packagr-bumpr
-
 if [[ ! -z "${CUSTOM_WORKING_DIRECTORY}" ]]; then
   cd "${CUSTOM_WORKING_DIRECTORY}"
 fi
